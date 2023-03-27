02a3409f33377d15e858ff5d25823d22f0611c35
Ubaid ur rehman <85988195+Ubaidsabir19@users.noreply.github.com>
2023-03-27 19:38:05 +0500

    Add files via upload

---
+++ obj.ts
@@ -0,0 +1,22 @@
+const person = {
+ name : "Ubaid",
+ city : "Lahore",
+ "education#" : "BSSE",
+}
+
+console.log(person.city);
+
+
+//if we have special character then we will access object from this method
+console.log(person["education#"]);
+
+
+
+// syncrones (aik code py dosri depend hy) problem(index 1 mein task resolve ni hoga jab tak module load ni hoga)
+// Asyncrones in ECMA Script resolve this issue.
+
+// npm install --save inquirer used to add node modules.
+// intall inquirer type npm i @type/inquirer
+// "type": "module",  add this in package.json
+// make calculater (user sy input leni hy aur us hisab sy hum ny operation perform krna hy).
+// user sy pochna hy vowel hy ya string hy. 
